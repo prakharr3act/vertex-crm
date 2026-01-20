2db664df453e7b29c1ee34b0495f7dd112d18d78
Prakhar Patel <prakhar.codes433@gmail.com>
2026-01-20 14:21:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-const inputBtn = document.getElementById("input-btn");
-const inputEl = document.getElementById("input-el")
 let myLeads = [];
-const ulEl = document.querySelector('#ul-el')
-const leadsFromLocalstorage = JSON.parse(localStorage.getItem("myLeads"))
-const dltBtn = document.getElementById("dlt-btn")
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("dlt-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-
-dltBtn.addEventListener("dblclick",function (){
-  localStorage.clear()
-  myLeads = [];
-  renderLeads()
-})
-
-
-if(leadsFromLocalstorage){
-  myLeads = leadsFromLocalstorage;
-  renderLeads();
-}
-inputBtn.addEventListener("click", function (){
-myLeads.push(inputEl.value);
-inputEl.value = "";
-localStorage.setItem("myLeads",JSON.stringify(myLeads) )
-renderLeads();
-})
-
-
-function renderLeads(){
-let listItems = ""
-for(let i = 0; i < myLeads.length ; i++){
-listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
-}
-ulEl.innerHTML = listItems;
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
 }
 
+function render(leads) {
+    let listItems = "";
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
+    }
+    ulEl.innerHTML = listItems;
+}
 
+deleteBtn.addEventListener("click", function() {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+});
+
+inputBtn.addEventListener("click", function() {
+    if (inputEl.value) {
+        myLeads.push(inputEl.value);
+        inputEl.value = "";
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    }
+});
